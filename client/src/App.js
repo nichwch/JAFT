b0c1b378bf6baef4a6dcc9e7557f20e8975ba89d
Nicholas Chen <nichwch@gmail.com>
2019-09-01 19:00:14 -0700

    timer time formatted correctly

--- client/src/App.js
+++ client/src/App.js
@@ -19,20 +19,74 @@ class App extends React.Component {
   {
     super(props);
     this.state = {
-      mode:HISTORY,
+      mode:TIMER,
+      startTime:0,
       time:0,
+      on:false,
       stamps:[],
-      tags:new Set(),
+      tags:["FB","Medium","Twitter"],
     }
+  }
+
+  startTimer = () =>
+  {
+    this.setState({
+      startTime:Date.now()-this.state.time,
+      time:this.state.time,
+      on:true,
+    });
+    this.timer = setInterval(()=>{
+      // console.log("now",moment(Date.now()).valueOf());
+      // console.log("start",moment(this.state.startTime).valueOf());
+      // console.log("subtract",moment(Date.now()).valueOf() - moment(this.state.startTime).valueOf());
+
+      //useful millisecond values to add on for testing
+      //35990000 (9 hours 59 minutes 50 seconds)
+      //590000 (9 minutes 50 seconds)
+
+
+      this.setState({
+        time:moment(Date.now()).valueOf() - moment(this.state.startTime).valueOf() + 35990000,
+      });
+    },1);
+
+  }
+
+  stopTimer = () => {
+    this.setState({
+      on:false,
+    })
+    clearInterval(this.timer);
+  }
+
+  resetTimer = () => {
+    this.setState({
+      time:0,
+      on:false,
+    })
   }
 
 
   render()
   {
-    let content = (<React.Fragment><Timer /></React.Fragment>);
+    let content = (<React.Fragment>
+      <Timer
+      controls={{start:this.startTimer,stop:this.stopTimer,reset:this.resetTimer}}
+      time={this.state.time}
+      on={this.state.on}
+      tags={this.state.tags}
+      />
+      </React.Fragment>);
     if(this.state.mode===TIMER)
     {
-      content = (<React.Fragment><Timer /></React.Fragment>);
+      content = (<React.Fragment>
+        <Timer
+        controls={{start:this.startTimer,stop:this.stopTimer,reset:this.resetTimer}}
+        time={this.state.time}
+        on={this.state.on}
+        tags={this.state.tags}
+        />
+        </React.Fragment>);
     }
     else if(this.state.mode===SESSION)
     {
--- client/src/Components/Timer.js
+++ client/src/Components/Timer.js
@@ -1,10 +1,42 @@
-import React from "react";
-
+import React,{useState,useEffect} from "react";
+var moment = require('moment');
 const Timer = (props) =>
 {
+  // useEffect(()=>{
+  //   props.controls.start();
+  // },[]);
+  let date = new Date(props.time);
+  let milliseconds = ((date.getUTCMilliseconds()+"").length>1?(date.getUTCMilliseconds()+"").substring(0,2):"0"+date.getUTCMilliseconds());
+  let seconds =((date.getUTCSeconds()+"").length>1?(date.getUTCSeconds()+"")+':':"0"+date.getUTCSeconds()+':');
+  let minutes = date.getUTCMinutes()>0?
+  ((date.getUTCMinutes()+"").length>1?(date.getUTCMinutes()+"")+':':"0"+date.getUTCMinutes()+':')
+  :
+  "";
+  let hours = date.getUTCHours()>0?
+  ((date.getUTCHours()+"").length>1?(date.getUTCHours()+"")+':':"0"+date.getUTCHours()+':')
+  :
+  "";
+  // if(format.split(':'))
   return (
     <React.Fragment>
-    Timer
+    {hours+minutes+seconds+milliseconds}
+    {
+      props.on?
+      <button onClick={props.controls.stop}>stop</button>
+      :
+      <button onClick={props.controls.start}>start</button>
+    }
+    {
+      ((!props.on)&&(props.time>0))?
+      <button onClick={props.controls.reset}>reset</button>
+      :
+      null
+    }
+    {props.tags.map((tag,i)=>{return (
+      <div key={i}>
+        {tag}
+      </div>
+    )})}
     </React.Fragment>
   )
 }
