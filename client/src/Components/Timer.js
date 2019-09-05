bee9a4d21702b4f0d83aa875e16e135b467ce869
Nicholas Chen <nichwch@gmail.com>
2019-09-04 21:15:07 -0700

    time format

--- client/src/Components/Timer.js
+++ client/src/Components/Timer.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from "react";
+import "./Timer.css";
 var moment = require('moment');
 const Timer = (props) =>
 {
@@ -6,37 +7,59 @@ const Timer = (props) =>
   //   props.controls.start();
   // },[]);
   let date = new Date(props.time);
-  let milliseconds = ((date.getUTCMilliseconds()+"").length>1?(date.getUTCMilliseconds()+"").substring(0,2):"0"+date.getUTCMilliseconds());
-  let seconds =((date.getUTCSeconds()+"").length>1?(date.getUTCSeconds()+"")+':':"0"+date.getUTCSeconds()+':');
-  let minutes = date.getUTCMinutes()>0?
-  ((date.getUTCMinutes()+"").length>1?(date.getUTCMinutes()+"")+':':"0"+date.getUTCMinutes()+':')
-  :
-  "";
+
   let hours = date.getUTCHours()>0?
-  ((date.getUTCHours()+"").length>1?(date.getUTCHours()+"")+':':"0"+date.getUTCHours()+':')
+  ((date.getUTCHours()+"").length>1?(date.getUTCHours()+"")+':' : "0"+date.getUTCHours()+':')
   :
   "";
+
+  let minutes = date.getUTCMinutes()>0?
+  ((date.getUTCMinutes()+"").length>1?(date.getUTCMinutes()+"")+':' : "0"+date.getUTCMinutes()+':')
+  :
+  "";
+
+  let seconds = ""
+  if(hours.length>0)
+  {
+    seconds = ((date.getUTCSeconds()+"").length>1?(date.getUTCSeconds()+"") : "0"+date.getUTCSeconds());
+  }
+  else
+  {
+    seconds =((date.getUTCSeconds()+"").length>1?(date.getUTCSeconds()+"")+':' : "0"+date.getUTCSeconds()+':');
+  }
+
+  let milliseconds = "";
+  if(hours.length>0)
+  {
+    milliseconds = "";
+  }
+  else
+  {
+    milliseconds = ((date.getUTCMilliseconds()+"").length>1?(date.getUTCMilliseconds()+"").substring(0,2) : "0"+date.getUTCMilliseconds());
+  }
+
+
+  let finalTime = hours+minutes+seconds+milliseconds;
   // if(format.split(':'))
   return (
     <React.Fragment>
-    {hours+minutes+seconds+milliseconds}
-    {
-      props.on?
-      <button onClick={props.controls.stop}>stop</button>
-      :
-      <button onClick={props.controls.start}>start</button>
-    }
-    {
-      ((!props.on)&&(props.time>0))?
-      <button onClick={props.controls.reset}>reset</button>
-      :
-      null
-    }
-    {props.tags.map((tag,i)=>{return (
-      <div key={i}>
-        {tag}
-      </div>
-    )})}
+        <div className = "circleTimerContainer" onClick={props.on?props.controls.stop:props.controls.start}>
+            <div className = "timerTime">
+              {finalTime}
+            </div>
+        </div>
+        {
+          ((!props.on)&&(props.time>0))?
+          <button onClick={props.controls.reset}>reset</button>
+          :
+          null
+        }
+
+        {props.tags.map((tag,i)=>{return (
+          <div key={i}>
+            {tag}
+          </div>
+        )})}
     </React.Fragment>
   )
 }
