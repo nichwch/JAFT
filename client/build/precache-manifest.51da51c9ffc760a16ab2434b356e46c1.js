3972be8db7cef51ad50aacf7564b26970b777b1a
Nicholas Chen <nichwch@gmail.com>
2019-09-10 21:53:27 -0700

    asdf

---
+++ client/build/precache-manifest.51da51c9ffc760a16ab2434b356e46c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c77cf3c9c22e576057260157c81d19d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63456d9df1e3bec0ea01",
+    "url": "/static/css/main.0142fff3.chunk.css"
+  },
+  {
+    "revision": "06b1e9ae29bd19ba4e32",
+    "url": "/static/js/2.82e3ea26.chunk.js"
+  },
+  {
+    "revision": "63456d9df1e3bec0ea01",
+    "url": "/static/js/main.5505d755.chunk.js"
+  },
+  {
+    "revision": "a97c4bfd271671e32f5e",
+    "url": "/static/js/runtime~main.3ef4b0f4.js"
+  }
+]);
